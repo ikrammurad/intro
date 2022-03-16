63f7174a362db094ccf0c221bc18d0df6da0d538
ikram <ikrammurad320@gmail.com>
2022-03-16 00:21:01 -0700

    this is the devil's world

---
+++ src/main/java/com/example/democlass/DamoclesApplication.java
@@ -0,0 +1,14 @@
+package com.example.democlass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DamoclesApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(DamoclesApplication.class, args);
+    }
+
+}
